5097ea6875df45239b9db0622feef95f8c77d432
MrBrooks00 <2239365303@qq.com>
2015-03-04 23:35:21 +0800

    1

---
+++ HelloWorld.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "time.h"
+#include "conio.h"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <Windows.h>
+
+#include "wx/wx.h"
+
+using namespace std;
+
+bool MyApp::OnInit() {
+    MyFrame *frame = new MyFrame(wxT("HelloWorld"), wxPoint(200, 99), wxSize(600, 400));
+    frame->Show(true);
+    return true;
+}
